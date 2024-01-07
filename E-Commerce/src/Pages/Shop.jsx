5813d6813d12c2c52cb2f104c6553737b659a40b
Ahmetyildiz00 <151578762+Ahmetyildiz00@users.noreply.github.com>
2024-01-07 18:42:05 +0300

    E-Commerce

---
+++ E-Commerce/src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Popular from '../components/Popular/Popular'
+import NewCollections from '../components/NewCollections/NewCollections'
+
+const Shop = () => {
+  return (
+    <div>
+      <Popular/>
+      <NewCollections/>
+
+    </div>
+  )
+}
+export default Shop
